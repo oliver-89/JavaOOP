8e524f0c0bcd9ef3213d62cea67606440b82050e
oliver-89 <wjr980729@163.com>
2020-06-21 21:26:12 +0800

    done

---
+++ src/model/Student.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Student {
+	private String name;
+	private int age;
+	private String gender;
+	private String idNo;
+	
+	public Student(String name, int age, String gender, String idNo) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.idNo = idNo;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getIdNo() {
+		return idNo;
+	}
+	public void setIdNo(String idNo) {
+		this.idNo = idNo;
+	}
+
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", gender=" + gender
+				+ ", idNo=" + idNo + "]";
+	}
+	
+	
+
+}
---
+++ src/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+import model.Student;
+
+public class test {
+	public static void main(String[] args){
+		Student stu = new Student("Alice",18,"Ů","430405199807292309");
+		System.out.println(stu);
+	}
+}
